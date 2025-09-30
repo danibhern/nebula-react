2f1114757c805d299b1b88e196f95f17b109ef34
danibhern <dan.barrerah@duocuc.cl>
2025-09-30 00:11:23 -0300

    commit first

--- src/components/organisms/First.jsx
+++ src/components/organisms/First.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { HashLink } from 'react-router-hash-link';
+import nebulaImg from '../img/nebula.png'; // ajusta la ruta si es necesario
 
 export default function First() {
   return (
     <section id="first">
-    <nav>
-      <div className="logo"></div>
+      <nav>
+        <div className="logo">
+          <img src={nebulaImg} alt="Nebula" />
+        </div>
         <HashLink smooth to="#first">Home</HashLink>
         <HashLink smooth to="#second">Quienes Somos</HashLink>
       </nav>
