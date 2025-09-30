63294c816170989cb2abc1f4deb2fc9c18ddd0e0
yesi <yes.jara@duocuc.cl>
2025-09-29 23:06:08 -0300

    archivo promo mes

---
+++ src/components/organisms/Promo_mes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Promo_mes(){
+    return (
+        <section class="promo-mes">
+            <h2>Promo por el mes de Septiembre</h2>
+            <article>
+            <img src="https://template.canva.com/EAE7TAO6TzU/3/0/1600w-eix57AHCv_k.jpg" alt="Producto del mes: Espresso" />
+            <div class="producto-info">
+                <h3>Espresso Tradicional</h3>
+                <p>Este mes de Septiembre prueba nuestro espresso hecho con granos 100% arábica, tostados al punto justo para darte un sabor fuerte y un aroma que te encantará. ¡Aprovecha de Lunes a Viernes hasta las 11:00hrs el 20% de descuento en desayunos para llevar!</p>
+            </div>
+            </article>
+        </section>
+    )
+}
+
