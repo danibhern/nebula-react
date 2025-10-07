bd288868c82a35d5a17bdb7202da69bee2053d6b
danibhern <dan.barrerah@duocuc.cl>
2025-10-06 21:23:18 -0300

    ediciones

--- src/components/pages/Carrito.jsx
+++ src/components/pages/Carrito.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { FaFacebookF, FaInstagram, FaTwitter, FaMapMarkerAlt, FaFileAlt, FaShieldAlt } from 'react-icons/fa';
 import AtomLink from '../atoms/AtomLink';
 import AtomButton from '../atoms/AtomButton';
 import "../../styles/Catalogo.css";
@@ -364,29 +365,33 @@ export default function Carrito() {
             </div>
 
             <footer>
-                <div className="footer-section">
-                    <h3>Redes Sociales</h3>
-                    <ul>
-                        <li><AtomLink to="#" aria-label="Facebook">Facebook</AtomLink></li>
-                        <li><AtomLink to="#" aria-label="Instagram">Instagram</AtomLink></li>
-                        <li><AtomLink to="#" aria-label="Twitter">Twitter</AtomLink></li>
-                    </ul>
-                </div>
-                <div className="footer-section">
-                    <h3>Sucursales</h3>
-                    <ul>
-                        <li>Calle Principal 123, Melipilla</li>
-                        <li>Avenida Central 456, Villarica</li>
-                        <li>Bulevar 789, Copiapó</li>
-                    </ul>
-                </div>
-                <div className="footer-section">
-                    <h3>Políticas</h3>
-                    <ul>
-                        <li><AtomLink to="#">Políticas de Envío</AtomLink></li>
-                        <li><AtomLink to="#">Términos y Condiciones</AtomLink></li>
-                    </ul>
-                </div>
+                        <div className="footer-section">
+                          <h3>Redes Sociales</h3>
+                          <ul>
+                            <li><a href="#" aria-label="Facebook"><FaFacebookF /> Facebook</a></li>
+                            <li><a href="#" aria-label="Instagram"><FaInstagram /> Instagram</a></li>
+                            <li><a href="#" aria-label="Twitter"><FaTwitter /> Twitter</a></li>
+                          </ul>
+                        </div>
+                        <div className="footer-section">
+                          <h3>Sucursales</h3>
+                          <ul>
+                            <li>
+                              <a href="https://www.google.com/maps/search/?api=1&query=Calle+Serrano+1105,+Melipilla" target="_blank" rel="noopener noreferrer" aria-label="Abrir ubicación en Google Maps">
+                                <FaMapMarkerAlt /> Calle Serrano 1105, Melipilla
+                              </a>
+                            </li>
+                            <li><FaMapMarkerAlt /> Avenida Central 456, Villarica</li>
+                            <li><FaMapMarkerAlt /> Bulevar 789, Copiapó</li>
+                          </ul>
+                        </div>
+                        <div className="footer-section">
+                          <h3>Políticas</h3>
+                          <ul>
+                            <li><a href="#"><FaFileAlt /> Políticas de Envío</a></li>
+                            <li><a href="#"><FaShieldAlt /> Términos y Condiciones</a></li>
+                          </ul>
+                        </div>
             </footer>
         </>
     );
