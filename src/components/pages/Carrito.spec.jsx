e1963835a3cd3607d004e105c098bf35407077af
danibhern <dan.barrerah@duocuc.cl>
2025-10-14 18:52:12 -0300

    test añadir y eliminar producto listos

---
+++ src/components/pages/Carrito.spec.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import Carrito from './Carrito';
+import Catalogo from './Catalogo';
+import { BrowserRouter } from 'react-router-dom';
+
+beforeEach(() => {
+  Storage.prototype.getItem = jest.fn(() => '[]'); 
+  Storage.prototype.setItem = jest.fn();
+});
+
+describe('Carrito - añadir y eliminar productos', () => {
+
+  it('añade un producto al carrito desde Catalogo', async () => {
+    render(
+      <BrowserRouter>
+        <Catalogo />
+      </BrowserRouter>
+    );
+
+    const botonAñadir = screen.getAllByText('Añadir al carrito')[0];
+    fireEvent.click(botonAñadir);
+
+    const mensajeConfirmacion = await screen.findByText(/añadido al carrito!/i);
+    expect(mensajeConfirmacion).toBeInTheDocument();
+  });
+
+
+  it('elimina un producto del carrito', async () => {
+    const mockCarrito = [
+      {
+        nombre: "Café Daroma",
+        descripcion: "Café en grano 250g",
+        precio: 5890,
+        img: "https://focusmood.coffee/cl/wp-content/uploads/2024/08/Envase-Arabico-Negro-1-600x450.png",
+        cantidad: 1,
+      },
+    ];
+    Storage.prototype.getItem = jest.fn(() => JSON.stringify(mockCarrito));
+
+    render(
+      <BrowserRouter>
+        <Carrito />
+      </BrowserRouter>
+    );
+
+    expect(await screen.findByText(/Café Daroma/i)).toBeInTheDocument();
+
+    const botonesEliminar = screen.getAllByRole('button', { name: /🗑️/i });
+    fireEvent.click(botonesEliminar[0]);
+
+
+    await waitFor(() => {
+      expect(screen.queryByText(/Café Daroma/i)).toBeNull();
+    });
+  });
+
+});
